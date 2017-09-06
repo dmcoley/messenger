3f3b8f1993fb6c790f3767811174a9a39f68c4ca
dmcoley <dmcoley@cs.washington.edu>
2017-09-05 19:41:38 -0700

    update port to work when deploying

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
-var port = 3000;
+var port = process.env.PORT || 3000;
 var users = [];
 
 server.listen(port, function () {
