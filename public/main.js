7420fd2d461346261da5776e542e1f6e2df659f5
dmcoley <dmcoley@cs.washington.edu>
2017-09-08 13:12:52 -0700

    wrap main in anonymous function

--- public/main.js
+++ public/main.js
@@ -1,78 +1,80 @@
-// Connecting to socket.io
-var socket = io();
+$( document ).ready(function() { // Connecting to socket.io
+	var socket = io();
+	$("#messages").scrollTop($("#messages")[0].scrollHeight);
 
-// The username is requested and sent to the server and display in the title
-var username = null;
-do {
-	username = prompt('What\'s your username?');
-} while (!username);
-socket.emit('new_client', username);
-addUser(username);
+	// The username is requested and sent to the server and display in the title
+	var username = null;
+	do {
+		username = prompt('What\'s your username?');
+	} while (!username);
+	socket.emit('new_client', username);
+	addUser(username);
 
-// When a message is received it's inserted in the page
-socket.on('message', function(data) {
-   insertMessage(data.username, data.message)
-})
+	// When a message is received it's inserted in the page
+	socket.on('message', function(data) {
+	   insertMessage(data.username, data.message)
+	})
 
-// When a new client connects, the information is displayed
-socket.on('new_client', function(username, all_users) {
-	insertMessage(username + " has joined the chat!");
-    updateUsers(all_users);
-})
+	// When a new client connects, the information is displayed
+	socket.on('new_client', function(username, all_users) {
+		insertMessage(username + " has joined the chat!");
+	    updateUsers(all_users);
+	})
 
-// Update the active users in the view when a client disconnects
-socket.on('disconnect_client', function(username, all_users) {
-	insertMessage(username + " has left the chat!");
-    updateUsers(all_users);
-})
+	// Update the active users in the view when a client disconnects
+	socket.on('disconnect_client', function(username, all_users) {
+		insertMessage(username + " has left the chat!");
+	    updateUsers(all_users);
+	})
 
-// Update the active users in the view when a new client logs in
-socket.on('login', function(all_users) {
-	updateUsers(all_users);
-})
+	// Update the active users in the view when a new client logs in
+	socket.on('login', function(all_users) {
+		updateUsers(all_users);
+	})
 
-// When the form is sent, the message is sent and displayed on the page
-$('#chat_form').submit(function () {
-   var message = $('#message').val();
-   socket.emit('message', message); 
-   insertMessage(username, message);
-   $('#message').val('').focus();
-   return false; // Blocks 'classic' sending of the form
+	// When the form is sent, the message is sent and displayed on the page
+	$('#chat_form').submit(function () {
+	   var message = $('#message').val();
+	   socket.emit('message', message); 
+	   insertMessage(username, message);
+	   $('#message').val('').focus();
+	   return false; // Blocks 'classic' sending of the form
+	});
+
+	// Adds a message to the page
+	function insertMessage(username, message) {
+	   // Update the Angular.js controller and eagerly apply it
+	   angular.element($('#chat_form')).scope().$apply(function() {
+	   	angular.element($('#chat_form')).scope().messages.push({
+	      Name: username,
+	      Message: message
+	    })});
+	} 
+
+	// Updates the users in the view to be the passed in collection of users
+	function updateUsers(usernames) {
+		// Remove old users
+		var listItems = $("#online_users li");
+		var prevUsers = [];
+		listItems.each(function(idx, li) {
+		    var curr = $(li);
+		    var name = curr.context.innerText;
+		    prevUsers.push(name);
+		    if (usernames.indexOf(name) == -1) {
+		    	curr.remove();
+		    }
+		});
+
+		// Add new users
+		usernames.forEach(function(user) {
+			if (prevUsers.indexOf(user) == -1) {
+				addUser(user);
+			}
+		});
+	}  
+
+	// Adds the given user to the view
+	function addUser(user) {
+		$('#online_users').append('<li><div class=chatbox__user--active><p>' + user + '</p></div></li>');
+	}
 });
-
-// Adds a message to the page
-function insertMessage(username, message) {
-   // Update the Angular.js controller and eagerly apply it
-   angular.element($('#chat_form')).scope().$apply(function() {
-   	angular.element($('#chat_form')).scope().messages.push({
-      Name: username,
-      Message: message
-    })});
-} 
-
-// Updates the users in the view to be the passed in collection of users
-function updateUsers(usernames) {
-	// Remove old users
-	var listItems = $("#online_users li");
-	var prevUsers = [];
-	listItems.each(function(idx, li) {
-	    var curr = $(li);
-	    var name = curr.context.innerText;
-	    prevUsers.push(name);
-	    if (usernames.indexOf(name) == -1) {
-	    	curr.remove();
-	    }
-	});
-
-	// Add new users
-	usernames.forEach(function(user) {
-		if (prevUsers.indexOf(user) == -1) {
-			addUser(user);
-		}
-	});
-}  
-
-// Adds the given user to the view
-function addUser(user) {
-	$('#online_users').append('<li><div class=chatbox__user--active><p>' + user + '</p></div></li>');
-}
